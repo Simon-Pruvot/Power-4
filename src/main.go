96bf95893d4896d8f38f1b1eac883d7af3b8b4a4
simon-beausire <simon.beausire@ynov.com>
2025-09-22 12:11:04 +0200

    upd

--- src/main.go
+++ src/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseFiles("template/index.html"))
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, nil)
+}
 
 func main() {
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":80", nil)
 }
