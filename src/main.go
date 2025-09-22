e56fd86eb5d27ba5f447965bf378d3bc325cb5fd
Simon-Pruvot <simon.pruvot@ynov.com>
2025-09-22 11:52:07 +0200

    h

--- src/main.go
+++ src/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 var tmpl = template.Must(template.ParseFiles("index.html"))
+
+func handler(w http.ResponseWritter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World !")
+}
 
 func main() {
 	http.HandleFunc("/", handler)
