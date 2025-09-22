31189d07d83cb59cd0a1c02dbacaa5acdb35890b
simon-beausire <simon.beausire@ynov.com>
2025-09-22 12:16:00 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -5,9 +5,8 @@ import (
 	"net/http"
 )
 
-var tmpl = template.Must(template.ParseFiles("template/index.html"))
-
 func handler(w http.ResponseWriter, r *http.Request) {
+	var tmpl = template.Must(template.ParseFiles("template/index.html"))
 	tmpl.Execute(w, nil)
 }
 
