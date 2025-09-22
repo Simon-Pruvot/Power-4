eef33d3cdccc25e9d647fc8618ebfb0309992f3b
Simon-Pruvot <simon.pruvot@ynov.com>
2025-09-22 11:48:01 +0200

    x

--- src/main.go
+++ src/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseFiles("index.html"))
 
 func main() {
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":80", nil)
 }
