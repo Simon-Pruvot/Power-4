4ace3d5d2ed2889d217b1ef2428e26ded08a56fe
simon-beausire <simon.beausire@ynov.com>
2025-09-23 09:20:28 +0200

    z

--- src/main.go
+++ src/main.go
@@ -14,11 +14,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func game(w http.ResponseWriter, r *http.Request, grille *pageData) {
-	var tmpl = template.Must(template.ParseFiles("template/game.html"))
-	tmpl.Execute(w, grille)
-}
-
 func play(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("src/template/play.html"))
 	tmpl.Execute(w, nil)
@@ -27,7 +22,6 @@ func play(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("CSS"))))
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/game", game)
 	http.HandleFunc("/page2", play)
 	http.ListenAndServe(":80", nil)
 }
