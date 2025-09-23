06885059165f7b8e193a9f65f20c7cb67d359b3d
simon-beausire <simon.beausire@ynov.com>
2025-09-23 11:25:40 +0200

    z

--- src/main.go
+++ src/main.go
@@ -8,20 +8,27 @@ import (
 type pageData struct {
 	grille [][]int
 }
-
+jeu:= pageData{{{0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0},}}
 func handler(w http.ResponseWriter, r *http.Request) {
 	var tmpl = template.Must(template.ParseFiles("template/index.html"))
 	tmpl.Execute(w, nil)
 }
 
 func play(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("src/template/play.html"))
+	var tmpl = template.Must(template.ParseFiles("template/play.html"))
 	tmpl.Execute(w, nil)
 }
 
 func main() {
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("CSS"))))
+	fs := http.FileServer(http.Dir("./images"))
+	http.Handle("/images/", http.StripPrefix("/images/", fs))
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/page2", play)
+	http.HandleFunc("/play", play)
 	http.ListenAndServe(":80", nil)
 }
