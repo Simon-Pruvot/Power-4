f0404b230ca418d61a2a2419536faa3ccaa70c41
simon-beausire <simon.beausire@ynov.com>
2025-09-29 09:13:40 +0200

    update

--- src/main.go
+++ src/main.go
@@ -6,7 +6,9 @@ import (
 )
 
 type pageData struct {
-	Grille [][]string
+	Grille       [][]string
+	joueur       []string
+	indiceJoueur int
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,15 +18,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func play(w http.ResponseWriter, r *http.Request) {
 	data := pageData{Grille: [][]string{
-		{"/images/pion3.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png"},
+		{"/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png"},
 		{"/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png"},
 		{"/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png"},
 		{"/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png"},
 		{"/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png"},
 		{"/images/pion0.png", "/images/pion0.png", "/images/pion2.png", "/images/pion1.png", "/images/pion0.png", "/images/pion0.png", "/images/pion0.png"},
-	}}
+	}, joueur: []string{"/images/pion1.png", "/images/pion2.png"}, indiceJoueur: 0}
 	tmpl := template.Must(template.ParseFiles("template/play.html"))
 	tmpl.Execute(w, data)
+}
+
+func (data *pageData) ajouterPion(index int) {
+	for i := len(data.Grille); i >= 0; i-- {
+		if data.Grille[i][index] == "/images/pion0.png" {
+			data.Grille[i][index] = data.joueur[data.indiceJoueur]
+		}
+	}
 }
 
 func main() {
